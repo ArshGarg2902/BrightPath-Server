1ab662be50be6305552db5bfd9a1e7f9846ce4f9
ArshGarg2902 <arshgarg958@gmail.com>
2025-05-11 12:40:05 +0530

    changes e

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 export const instance = new Razorpay({
   key_id: process.env.Razorpay_Key,
-  key_secret: process.env.Razorpay_Secret,
+  key_secret: process.env.Razorpay_secret,
 });
 
 const app = express();
